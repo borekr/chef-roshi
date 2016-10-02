0c85fe3017830d9ca8ac704d801c64974b8d5114
Rob Borek <rborek@gmail.com>
2016-10-02 11:29:57 -0400

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'roshi'
+maintainer       'Rob Borek'
+maintainer_email 'rborek@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures roshi'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
